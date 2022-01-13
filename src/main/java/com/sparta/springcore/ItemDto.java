a8a190f65d227f96f58df2a63ca7cdd5314cd95c
thdefn <80521474+thdefn@users.noreply.github.com>
2022-01-13 18:54:38 +0900

    Initial commit

---
+++ src/main/java/com/sparta/springcore/ItemDto.java
@@ -0,0 +1,12 @@
+package com.sparta.springcore;
+
+import lombok.Getter;
+
+@Getter
+public class ItemDto {
+    private String title;
+    private String link;
+    private String image;
+    private int lprice;
+}
+
