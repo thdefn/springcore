1f8cd882ccf6d361361a4f6e7ba8021f4f2c1ff0
thdefn <80521474+thdefn@users.noreply.github.com>
2022-01-16 17:33:19 +0900

    [refactor] make oop

--- src/main/java/com/sparta/springcore/ProductRepository.java
+++ src/main/java/com/sparta/springcore/ProductRepository.java
@@ -1,29 +1,19 @@
 package com.sparta.springcore;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
-@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
-@RestController // JSON으로 데이터를 주고받음을 선언합니다.
-public class AllInOneController {
+public class ProductRepository {
 
-    // 신규 상품 등록
-    @PostMapping("/api/products")
-    public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
-// 요청받은 DTO 로 DB에 저장할 객체 만들기
-        Product product = new Product(requestDto);
-
-// DB 연결
+    public void createProduct(Product product) throws SQLException {
+        // DB 연결
         Connection connection = DriverManager.getConnection("jdbc:h2:mem:springcoredb", "sa", "");
 
-// DB Query 작성
+        // DB Query 작성
         PreparedStatement ps = connection.prepareStatement("select max(id) as id from product");
         ResultSet rs = ps.executeQuery();
         if (rs.next()) {
-// product id 설정 = product 테이블의 마지막 id + 1
+            // product id 설정 = product 테이블의 마지막 id + 1
             product.setId(rs.getLong("id") + 1);
         } else {
             throw new SQLException("product 테이블의 마지막 id 값을 찾아오지 못했습니다.");
@@ -36,30 +26,25 @@ public class AllInOneController {
         ps.setInt(5, product.getLprice());
         ps.setInt(6, product.getMyprice());
 
-// DB Query 실행
+        // DB Query 실행
         ps.executeUpdate();
 
-// DB 연결 해제
+        // DB 연결 해제
         ps.close();
         connection.close();
-
-// 응답 보내기
-        return product;
     }
 
-    // 설정 가격 변경
-    @PutMapping("/api/products/{id}")
-    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
-        Product product = new Product();
-
-// DB 연결
+    public Product getProduct(Long id) throws SQLException {
+        // DB 연결
         Connection connection = DriverManager.getConnection("jdbc:h2:mem:springcoredb", "sa", "");
 
-// DB Query 작성
+        // DB Query 작성
         PreparedStatement ps = connection.prepareStatement("select * from product where id = ?");
         ps.setLong(1, id);
 
-// DB Query 실행
+        Product product = new Product();
+
+        // DB Query 실행
         ResultSet rs = ps.executeQuery();
         if (rs.next()) {
             product.setId(rs.getLong("id"));
@@ -72,27 +57,30 @@ public class AllInOneController {
             throw new NullPointerException("해당 아이디가 존재하지 않습니다.");
         }
 
-// DB Query 작성
-        ps = connection.prepareStatement("update product set myprice = ? where id = ?");
-        ps.setInt(1, requestDto.getMyprice());
-        ps.setLong(2, product.getId());
-
-// DB Query 실행
-        ps.executeUpdate();
-
-// DB 연결 해제
         rs.close();
-        ps.close();
-        connection.close();
 
-// 응답 보내기 (업데이트된 상품 id)
-        return product.getId();
+        return product;
     }
 
-    // 등록된 전체 상품 목록 조회
-    @GetMapping("/api/products")
-    public List<Product> getProducts() throws SQLException {
-        List<Product> products = new ArrayList<>();
+    public void updateProduct(Long id, int myprice) throws SQLException {
+        // DB 연결
+        Connection connection = DriverManager.getConnection("jdbc:h2:mem:springcoredb", "sa", "");
+
+        // DB Query 작성
+        PreparedStatement ps = connection.prepareStatement("update product set myprice = ? where id = ?");
+        ps.setInt(1, myprice);
+        ps.setLong(2, id);
+
+        // DB Query 실행
+        ps.executeUpdate();
+
+        // DB 연결 해제
+        //rs.close();
+        ps.close();
+        connection.close();
+    }
+
+    public void getProducts(List<Product> products) throws SQLException {
 
 // DB 연결
         Connection connection = DriverManager.getConnection("jdbc:h2:mem:springcoredb", "sa", "");
@@ -116,8 +104,5 @@ public class AllInOneController {
 // DB 연결 해제
         rs.close();
         connection.close();
-
-// 응답 보내기
-        return products;
     }
 }
