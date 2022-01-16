8473fa332f8cfd0a0c3eb97db3c5ec0d1859f9c3
thdefn <80521474+thdefn@users.noreply.github.com>
2022-01-16 20:01:22 +0900

    [refactor] make DI

--- src/main/java/com/sparta/springcore/controller/ProductController.java
+++ src/main/java/com/sparta/springcore/controller/ProductController.java
@@ -1,30 +1,31 @@
-package com.sparta.springcore;
+package com.sparta.springcore.controller;
 
-import lombok.RequiredArgsConstructor;
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.dto.ProductMypriceRequestDto;
+import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.sql.*;
-import java.util.ArrayList;
+
+import java.sql.SQLException;
 import java.util.List;
 
-@RequiredArgsConstructor // final로 선언된 멤버 변수를 자동으로 생성합니다.
 @RestController // JSON으로 데이터를 주고받음을 선언합니다.
 public class ProductController {
 
     private final ProductService productService;
 
-    public ProductController(){
-        ProductService productService = new ProductService();
+    @Autowired
+    public ProductController(ProductService productService) {
         this.productService = productService;
-
     }
 
     // 신규 상품 등록
     @PostMapping("/api/products")
     public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
+        Product product = productService.createProduct(requestDto);
 
-        Product product =  productService.createProduct(requestDto);
-
-        // 응답 보내기
+// 응답 보내기
         return product;
     }
 
@@ -33,7 +34,7 @@ public class ProductController {
     public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
         Product product = productService.updateProduct(id, requestDto);
 
-        // 응답 보내기 (업데이트된 상품 id)
+// 응답 보내기 (업데이트된 상품 id)
         return product.getId();
     }
 
@@ -42,7 +43,7 @@ public class ProductController {
     public List<Product> getProducts() throws SQLException {
         List<Product> products = productService.getProducts();
 
-        // 응답 보내기
+// 응답 보내기
         return products;
     }
 }
