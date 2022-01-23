15d3fa3144644401b65b243283462a1798043a81
thdefn <80521474+thdefn@users.noreply.github.com>
2022-01-23 14:48:01 +0900

    [feat] implement kakao register and login

--- src/main/java/com/sparta/springcore/controller/UserController.java
+++ src/main/java/com/sparta/springcore/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sparta.springcore.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.springcore.dto.SignupRequestDto;
+import com.sparta.springcore.service.KakaoUserService;
 import com.sparta.springcore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 
     private final UserService userService;
+    private final KakaoUserService kakaoUserService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, KakaoUserService kakaoUserService) {
         this.userService = userService;
+        this.kakaoUserService = kakaoUserService;
     }
 
     // 회원 로그인 페이지
@@ -40,7 +43,7 @@ public class UserController {
 
     @GetMapping("/user/kakao/callback")
     public String kakaoLogin(@RequestParam String code) throws JsonProcessingException {
-        userService.kakaoLogin(code);
+        kakaoUserService.kakaoLogin(code);
         return "redirect:/";
     }
 }
--- src/main/java/com/sparta/springcore/dto/KakaoUserInfoDto.java
+++ src/main/java/com/sparta/springcore/dto/KakaoUserInfoDto.java
@@ -1,8 +1,10 @@
 package com.sparta.springcore.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class KakaoUserInfoDto {
     private Long id;
     private String nickname;
